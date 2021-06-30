ff36dbd51cb74891b2fa67ccf724b4ed1d417720
swren02 <swren02@gmail.com>
2021-06-30 15:02:52 -0400

    Fixed environment variables and authentification for sheets

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const http = require('http');
@@ -27,13 +28,18 @@ server.listen(3000, () => {
 function peekSpreadsheet(range) {
     console.log("In peekSpreadsheet");
     const {google} = require('googleapis');
-    const auth = google.auth.getClient({ scopes : ['https://www.googleapis.com/auth/spreadsheets.readonly']});
-    const sheets = google.sheets({version : 'v4', auth});
+    /*google.load("client:auth2", function() {
+      google.auth2.init({client_id: process.env.GOOGLE_APPLICATION_CREDENTIALS.client_id});
+    });*/
+
+    //const auth = google.auth.getClient({ scopes : ['https://www.googleapis.com/auth/spreadsheets.readonly']});
+    const sheets = google.sheets('v4');
     console.log(process.env.SPREADSHEET_ID);
     console.log("test3");
     sheets.spreadsheets.values.get({
-      spreadsheetId : '1TPbkDX5qDWCXruL6Kv6lRVfSvgGXxeouPUlVM9STw7A', //CHANGE THIS HUGE SECURITY FLAW
-      range : 'Sheet1!A1:B2',
+      spreadsheetId : process.env.SPREADSHEET_ID,
+      range : range,
+      key : process.env.API_KEY,
     }, (err, result) => {
       if (err) {
         // Handle error
