dade39c4678b94a3abe4e118ea63e7b6f8e4860b
swren02 <swren02@gmail.com>
2021-07-05 10:56:20 -0400

    Switched from sending out USDT to ETH

--- index.js
+++ index.js
@@ -188,16 +188,12 @@ function payoutAddress(address, payout) {
     // If your token is divisible to 8 decimal places, 42 = 0.00000042 of your token
     var transferAmount = payout;
 
-    // Determine the nonce
-    var count = await web3.eth.getTransactionCount(myAddress);
-    console.log(`num transactions so far: ${count}`);
-
-    // This file is just JSON stolen from the contract page on etherscan.io under "Contract ABI"
+   /* // This file is just JSON stolen from the contract page on etherscan.io under "Contract ABI"
     var abiArray = JSON.parse(fs.readFileSync(path.resolve(__dirname, './tt3.json'), 'utf-8'));
 
     // This is the address of the contract which created the ERC20 token
     var contractAddress = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
-    var contract = new web3.eth.Contract(abiArray, contractAddress, { from: myAddress });
+    var contract = new web3.eth.Contract(abiArray, contractAddress, { from: myAddress });*/
 
     // How many tokens do I have before sending?
     var balance = await contract.methods.balanceOf(myAddress).call();
@@ -206,18 +202,18 @@ function payoutAddress(address, payout) {
     // I chose gas price and gas limit based on what ethereum wallet was recommending for a similar transaction. You may need to change the gas price!
     var rawTransaction = {
         "from": myAddress,
-        "nonce": "0x" + count.toString(16),
+        "to": contractAddress,
+        //"nonce": "0x" + count.toString(16),
         "gasPrice": "0x003B9ACA00",
         "gasLimit": "0x250CA",
-        "to": contractAddress,
-        "value": "0x0",
-        "data": contract.methods.transfer(destAddress, transferAmount).encodeABI(),
+        "value": transferAmount,
+        //"data": contract.methods.transfer(destAddress, transferAmount).encodeABI(),
         "chainId": 0x03 //CHANGE THIS FOR TESTING, FOR DEPLOYMENT USE 0x01
     };
 
     // Example private key (do not use): 'e331b6d69882b4cb4ea581d88e0b604039a3de5967688d3dcffdd2270c0fd109'
     // The private key must be for myAddress
-    var privKey = new Buffer(my_privkey, 'hex');
+    var privKey = process.env.PRIVATE_KEY;
     var tx = new Tx(rawTransaction);
     tx.sign(privKey);
     var serializedTx = tx.serialize();
@@ -226,12 +222,7 @@ function payoutAddress(address, payout) {
     console.log(`Attempting to send signed tx:  ${serializedTx.toString('hex')}`);
     var receipt = await web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'));
     console.log(`Receipt info:  ${JSON.stringify(receipt, null, '\t')}`);
-
-    // The balance may not be updated yet, but let's check
-    balance = await contract.methods.balanceOf(myAddress).call();
-    console.log(`Balance after send: ${balance}`);
   }
-
 }
 
 /**
